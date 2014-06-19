90b718f5d4432cddf911836e293ae733f237b320
Dominic Filipek <dominicfvr@gmail.com>
2014-06-19 02:23:23 -0500

    Update to v1.5
    Update to v1.5

--- js/intrinsic.js
+++ js/intrinsic.js
@@ -5,13 +5,12 @@ function playSomeMusic(myGenre) {
 
 	SC.get('/tracks', { genres: myGenre, bpm: { from: 120 } },
   	function(tracks) {
-    //FIRST soundcloud sends back an array of 50 objects (0~49)
-    //so let's randomize which one we play:
-    //this logic returns a number between 0 and 50
+    //SoundCloud sends back an array of 50 tracks so let's randomize 
+    //which of the 50 is played first
     var random = Math.floor(Math.random() * 49);
     var track_url = tracks[random].permalink_url;
-    //LAST let's use the oEmbed method to display the soundplayer
-    //in the element #target with a options such as auto play
+    //The oEmbed method is utilized to display the player
+    //in the element #target with options such as auto play
     SC.oEmbed(track_url, {auto_play: true, iframe: true, show_comments: true, color: "ff0066"},
       document.getElementById("target"));
 		}
